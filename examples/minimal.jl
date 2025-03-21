4956a184e95484aa67dbd15f249ca980fd644cdb
Thomas <t.o.c.cheng@student.tudelft.nl>
2025-03-21 14:38:30 +0100

    Added GitHub CI run instructions

--- examples/minimal.jl
+++ examples/minimal.jl
@@ -2,9 +2,10 @@ using MeshViewer
 using Elementa
 using Elementa.GmshReader
 
-pg, n, e, et = Elementa.GmshReader.readfile("test/input/50x50-tri.msh")
+pg, n, e, et = Elementa.GmshReader.readfile("test/input/250x1000-tri.msh")
 elements = e[end]
 mesh = Mesh(pg, n, e, et, 2)
 Elementa.create_connectivity!(mesh, 1, 0)
 vertices = [create_line_vertices(mesh, 1), create_tri_vertices(mesh, 2)]
 viewer(vertices)
+
